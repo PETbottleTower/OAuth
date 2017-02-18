6322fe06678e05d00a1a0f1693a8e8b49648fced
Konomi Mori <morikonomi7@gmail.com>
2017-02-18 18:47:42 +0900

    revision hash

--- http.rb
+++ http.rb
@@ -19,7 +19,8 @@ def post (url)
   end
 end
 
-def authorization_header (oauth_consumer_key, oauth_token, oauth_signature_method, oauth_signature, oauth_timestamp, oauth_nonce)
+# oauth_signatureは含まないAuthorizationヘッダを作成
+def authorization_header (oauth_consumer_key, oauth_token, oauth_signature_method, oauth_timestamp, oauth_nonce)
   =begin
   $oauth_consumer_key
   $oauth_token
@@ -34,11 +35,19 @@ def authorization_header (oauth_consumer_key, oauth_token, oauth_signature_metho
   oauth_consumer_key = "oauth_consumer_key=\"" + url_encode(oauth_consumer_key) + "\","
   oauth_token = "oauth_token=\"" + url_encode(oauth_token) + "\","
   oauth_signature_method = "oauth_signature_method=\"" + url_encode(oauth_signature_method) + "\","
-  oauth_signature = "oauth_signature=\"" + url_encode(oauth_signature) + "\","
   oauth_timestamp = "oauth_timestamp=\"" + url_encode(oauth_timestamp) + "\","
   oauth_nonce = "oauth_nonce=\"" + url_encode(oauth_nonce) + "\""
 
-  header["auth_header"] = auth_scheme + oauth_consumer_key + oauth_token + oauth_signature_method + oauth_signature + oauth_timestamp + oauth_nonce
+  hash = {"auth_header" => auth_scheme + oauth_consumer_key + oauth_token + oauth_signature_method + oauth_timestamp + oauth_nonce}
+end
+
+def add_signature_to_authorization (oauth_signature)
+  oauth_signature = "oauth_signature=\"" + url_encode(oauth_signature) + "\""
+
+  header = authorization_header.dup
+
+  header["auth_header"] = header["auth_header"] + oauth_signature
+
 end
 
 end
